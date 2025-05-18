7cb15392482cbef90f07423d6aed0d5fb22cc11b
miura <shinji65536@gmail.com>
2025-05-18 20:09:41 +0900

    jwtの検証処理追加

--- router/router.go
+++ router/router.go
@@ -2,16 +2,31 @@ package router
 
 import (
 	"github.com/shinsx/golang-blog/controller"
-	"net/http"
 
+	"net/http"
+	"os"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
 func NewRouter(uc controller.IUserController) *echo.Echo {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
-	})
+	e.GET("/", Hello)
 	e.POST("/login", uc.LogIn)
+	t := e.Group("/articles")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", LogInCheck)
 	return e
+}
+
+func Hello(c echo.Context) error {
+  return c.String(http.StatusOK, "Hello, World!")
+}
+
+func LogInCheck(c echo.Context) error {
+  return c.String(http.StatusOK, "ログインしてる")
 }
